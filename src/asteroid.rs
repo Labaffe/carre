466ca6c884efc1d1cd1ebdedf97c5ea2dff3be89
Labaffe <lounis.lemaitre@gmail.com>
2026-04-08 21:37:26 +0200

    les asteroids sont tirées aléatoirement

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -7,10 +7,12 @@ pub struct AsteroidPlugin;
 
 impl Plugin for AsteroidPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(AsteroidSpawner::default()).add_systems(
-            Update,
-            (spawn_asteroids, move_asteroids).run_if(in_state(GameState::Playing)),
-        );
+        app.insert_resource(AsteroidSpawner::default())
+            .add_systems(Startup, preload_asteroid_textures)
+            .add_systems(
+                Update,
+                (spawn_asteroids, move_asteroids).run_if(in_state(GameState::Playing)),
+            );
     }
 }
 
@@ -21,6 +23,9 @@ pub struct Asteroid {
     pub health: i32,
     pub size: Vec2,
 }
+
+#[derive(Resource)]
+struct AsteroidTextures(Vec<Handle<Image>>);
 
 #[derive(Resource)]
 struct AsteroidSpawner {
@@ -35,64 +40,67 @@ impl Default for AsteroidSpawner {
     }
 }
 
+fn preload_asteroid_textures(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let handles = (0..=16)
+        .map(|i| asset_server.load(format!("images/asteroids/asteroid_x{:03}.png", i)))
+        .collect();
+    commands.insert_resource(AsteroidTextures(handles));
+}
+
 fn spawn_asteroids(
     windows: Query<&Window>,
     mut commands: Commands,
     mut spawner: ResMut<AsteroidSpawner>,
     time: Res<Time>,
-    asset_server: Res<AssetServer>,
     difficulty: Res<Difficulty>,
+    textures: Res<AsteroidTextures>,
 ) {
-    // ajuster l'intervalle de spawn selon la difficulté
-    spawner
-        .timer
-        .set_duration(Duration::from_secs_f32(difficulty.spawn_interval()));
+    spawner.timer.set_duration(Duration::from_secs_f32(difficulty.spawn_interval()));
     spawner.timer.tick(time.delta());
 
-    if spawner.timer.just_finished() {
-        let window = windows.single();
-        let x = fastrand::f32() * window.width() - window.width() / 2.0;
-        let y = 500.0;
+    if !spawner.timer.just_finished() {
+        return;
+    }
 
-        let is_small = fastrand::bool();
+    let window = windows.single();
+    let x = fastrand::f32() * window.width() - window.width() / 2.0;
 
-        let transform = Transform::from_xyz(x, y, 0.0).with_rotation(Quat::from_rotation_z(
-            fastrand::f32() * std::f32::consts::TAU,
-        ));
+    let is_small = fastrand::bool();
+    let texture = textures.0[fastrand::usize(..textures.0.len())].clone();
 
-        let (image, size, radius, health, base_velocity) = if is_small {
-            (
-                "images/asteroid_1.png",
-                Vec2::new(24.0 * 2.0, 24.0 * 2.0),
-                20.0,
-                1,
-                Vec3::new(0.0, -120.0 * (fastrand::f32() + 1.0), 0.0),
-            )
-        } else {
-            (
-                "images/asteroid_2.png",
-                Vec2::new(41.0 * 2.5, 41.0 * 2.5),
-                47.0,
-                5,
-                Vec3::new(0.0, -100.0 * (fastrand::f32() + 1.0), 0.0),
-            )
-        };
+    let transform = Transform::from_xyz(x, 500.0, 0.0)
+        .with_rotation(Quat::from_rotation_z(fastrand::f32() * std::f32::consts::TAU));
 
-        let velocity = base_velocity * difficulty.factor;
+    let (size, radius, health, base_velocity) = if is_small {
+        (
+            Vec2::new(48.0, 48.0),
+            20.0,
+            1,
+            Vec3::new(0.0, -120.0 * (fastrand::f32() + 1.0), 0.0),
+        )
+    } else {
+        (
+            Vec2::new(102.5, 102.5),
+            47.0,
+            5,
+            Vec3::new(0.0, -100.0 * (fastrand::f32() + 1.0), 0.0),
+        )
+    };
 
-        commands.spawn((
-            SpriteBundle {
-                texture: asset_server.load(image),
-                sprite: Sprite {
-                    custom_size: Some(size),
-                    ..default()
-                },
-                transform,
+    let velocity = base_velocity * difficulty.factor;
+
+    commands.spawn((
+        SpriteBundle {
+            texture,
+            sprite: Sprite {
+                custom_size: Some(size),
                 ..default()
             },
-            Asteroid { velocity, radius, health, size },
-        ));
-    }
+            transform,
+            ..default()
+        },
+        Asteroid { velocity, radius, health, size },
+    ));
 }
 
 fn move_asteroids(mut query: Query<(&mut Transform, &Asteroid)>, time: Res<Time>) {
